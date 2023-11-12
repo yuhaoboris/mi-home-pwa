81a9b3c1a60c9c9d195aafb6fea717c135a3433e
boris <yuhaoboris@gmail.com>
2023-11-12 17:46:11 +0800

    remove @vite-pwa/nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,9 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
         { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
-        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' }
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/pwa-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/pwa-512x512.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }
       ]
     }
   },
@@ -30,7 +30,6 @@ export default defineNuxtConfig({
   ],
 
   modules: [
-    '@vite-pwa/nuxt',
     '@nuxt/ui',
     'nuxt-icon',
     '@nuxt/image'
