05fdd83352b1e39d6deca5775a8037aa1d98e840
boris <yuhaoboris@gmail.com>
2023-11-15 18:00:04 +0800

    remove @vite-pwa/nuxt module

--- app.vue
+++ app.vue
@@ -1,6 +1,4 @@
 <template>
-  <VitePwaManifest></VitePwaManifest>
-  
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,14 +10,14 @@ export default defineNuxtConfig({
         { name: 'keywords', content: '物聯網,智慧插座' },
         { name: 'theme-color', content: '#28A7E1' }
       ],
-      // link: [
-      //   { rel: 'icon', href: '/favicon.ico' },
-      //   { rel: 'icon', sizes: '64x64', href: '/pwa-64x64.png' },
-      //   { rel: 'icon', sizes: '192x192', href: '/pwa-192x192.png' },
-      //   { rel: 'icon', sizes: '512x512', href: '/pwa-512x512.png' },
-      //   { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' },
-      //   { rel: 'manifest', href: '/manifest.webmanifest' }
-      // ]
+      link: [
+        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'icon', sizes: '64x64', href: '/pwa-64x64.png' },
+        { rel: 'icon', sizes: '192x192', href: '/pwa-192x192.png' },
+        { rel: 'icon', sizes: '512x512', href: '/pwa-512x512.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon-180x180.png' },
+        { rel: 'manifest', href: '/manifest.webmanifest' }
+      ]
     }
   },
 
@@ -33,56 +33,8 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/ui',
     'nuxt-icon',
-    '@nuxt/image',
-    '@vite-pwa/nuxt'
+    '@nuxt/image'
   ],
-
-  pwa: {
-    strategies: 'injectManifest',
-    // registerType: 'autoUpdate',
-    srcDir: 'public',
-    filename: 'sw.js',
-    scope: '/',
-    manifest: {
-      name: 'UEC Smart Plug',
-      short_name: 'UEC Plug',
-      description: 'UEC Smart Plug，隨時隨地看護用電安全',
-      background_color: '#28A7E1',
-      theme_color: '#28A7E1',
-      display: 'standalone',
-      icons: [
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        },
-        {
-          src: 'maskable-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'any maskable'
-        }
-      ]
-    },
-    // workbox: {
-    //   navigateFallback: null,
-    //   globPatterns: ['*/**.{js,css,html,png,jpg,svg,ico}']
-    // },
-    // client: {
-    //   installPrompt: true
-    // },
-    // periodicSyncForUpdates: 3600,
-    devOptions: {
-      enabled: true,
-      type: 'module',
-      navigateFallbackAllowlist: [/^\/$/]
-    }
-  },
 
   postcss: {
     plugins: {
